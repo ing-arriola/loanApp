7e523f9a1b6932bc0aaf20fd1e5cd3059bf95742
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-21 10:42:56 -0600

    Hello world in React is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello from react</h1>
     </div>
   );
 }
