576f0e945a954a4af76e1836a4598c498f59c1da
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-21 12:08:07 -0600

    Refactoring component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,7 @@
 import React from 'react';
-
-function Header(props){
-    return(
-
-    <h1>{props.title}</h1>
-    )
-}
+//created with sfc snnipet
+const Header = ({title}) => (
+    <h1>{title}</h1>
+)      
 
 export default Header
